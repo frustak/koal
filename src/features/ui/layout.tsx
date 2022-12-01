26dcb1ba693cea15c07757421448eedfff03a73a
frustak <soulburn0017@gmail.com>
2022-12-01 22:44:03 +0330

    improve layout design

--- src/features/ui/layout.tsx
+++ src/features/ui/layout.tsx
@@ -1,5 +1,11 @@
 import type { ReactNode } from "react";
 
-export function Layout({ children }: { children: ReactNode }) {
-  return <div className="font-display text-neutral-700">{children}</div>;
-}
+export const Layout = ({ children }: { children: ReactNode }) => {
+  return (
+    <div className="container mx-auto flex h-screen  flex-col p-8 font-display text-neutral-700">
+      <div className="grow rounded-sm border-4 border-neutral-700 px-6 py-4">
+        {children}
+      </div>
+    </div>
+  );
+};
