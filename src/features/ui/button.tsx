e97b3f82f63ffd275e15fe515fe14230f37751dc
frustak <soulburn0017@gmail.com>
2022-12-04 23:57:52 +0330

    keep previous button class when passing class prop

--- src/features/ui/button.tsx
+++ src/features/ui/button.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import type { ButtonHTMLAttributes } from "react";
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
@@ -5,8 +6,11 @@ type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
 export const Button = (props: ButtonProps) => {
   return (
     <button
-      className="border-x-4 border-y border-neutral-700 px-2 py-1 font-medium hover:bg-neutral-50"
       {...props}
+      className={clsx(
+        "flex items-center justify-center rounded-sm border-x-4 border-y border-neutral-700 px-2 py-1 font-medium hover:bg-neutral-50",
+        props.className
+      )}
     />
   );
 };
