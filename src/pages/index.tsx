4a4e6971008b83d10f9fbff3d3534a2be779ce44
frustak <soulburn0017@gmail.com>
2022-12-04 23:58:40 +0330

    remove dead home page code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,7 @@
 import { type NextPage } from "next";
 import Head from "next/head";
-import { useState } from "react";
-import { Profile } from "../features/auth/profile";
-import { trpc } from "../utils/trpc";
+import { Button } from "../features/ui/button";
+import { Header } from "../features/ui/header";
 
 const Home: NextPage = () => {
   return (
@@ -14,51 +13,11 @@ const Home: NextPage = () => {
       </Head>
 
       <main>
-        <div className="flex justify-end">
-          <Profile />
-        </div>
-        <GoalsList />
-        <GoalSubmitForm />
+        <Header />
+        <Button className="text-lg">New Day</Button>
       </main>
     </>
   );
 };
 
 export default Home;
-
-const GoalsList = () => {
-  const goalsQuery = trpc.todo.getGoals.useQuery();
-
-  if (goalsQuery.isLoading) return <div> fetching goals </div>;
-
-  return (
-    <div>
-      Your goals: {goalsQuery.data?.goals.map((goal) => `${goal.name} , `)}
-    </div>
-  );
-};
-
-const GoalSubmitForm = () => {
-  const [message, setMessage] = useState("");
-  const createGoal = trpc.todo.createGoal.useMutation();
-
-  return (
-    <form
-      onSubmit={(event) => {
-        event.preventDefault();
-        createGoal.mutate({ name: message });
-        setMessage("");
-      }}
-    >
-      <input
-        type="text"
-        value={message}
-        placeholder="Goal name"
-        minLength={2}
-        maxLength={100}
-        onChange={(event) => setMessage(event.target.value)}
-      />
-      <button type="submit">Submit</button>
-    </form>
-  );
-};
