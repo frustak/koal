734d987a313fba4907f98eba0bc4ba8748f88247
frustak <soulburn0017@gmail.com>
2022-12-04 23:32:42 +0330

    improve text selection color

--- src/features/ui/layout.tsx
+++ src/features/ui/layout.tsx
@@ -2,7 +2,7 @@ import type { ReactNode } from "react";
 
 export const Layout = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="container mx-auto flex h-screen  flex-col p-8 font-display text-neutral-700">
+    <div className="container mx-auto flex h-screen  flex-col p-8 font-display text-neutral-700 selection:bg-neutral-700 selection:text-white">
       <div className="grow rounded-sm border-4 border-neutral-700 px-6 py-4">
         {children}
       </div>
