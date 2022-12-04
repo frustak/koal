c0c628ccb302d7ae7a6e56af2146255fe635d535
frustak <soulburn0017@gmail.com>
2022-12-04 23:58:01 +0330

    minor refactor

--- src/features/auth/profile.tsx
+++ src/features/auth/profile.tsx
@@ -7,9 +7,9 @@ export const Profile = () => {
   return (
     <div className="flex items-center gap-6">
       <div className="text-sm">{session.data?.user?.name}</div>
-      <div className="text-xs">
-        <Button onClick={() => signOut()}>Sign Out</Button>
-      </div>
+      <Button className="text-xs" onClick={() => signOut()}>
+        Sign Out
+      </Button>
     </div>
   );
 };
