21e62cf9136d23ec391c911bbaea8dbbd1f9c2da
frustak <soulburn0017@gmail.com>
2022-12-05 21:14:01 +0330

    improve button components

--- src/features/ui/button.tsx
+++ src/features/ui/button.tsx
@@ -1,28 +1,68 @@
 import clsx from "clsx";
 import type { ButtonHTMLAttributes } from "react";
+import { Loader } from "./loader";
 
-type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+type BaseButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  loading?: boolean;
+};
 
-export const Button = (props: ButtonProps) => {
+type ButtonProps = BaseButtonProps & {
+  variant?: "default" | "filled";
+};
+
+export const Button = ({
+  children,
+  loading,
+  className,
+  variant = "default",
+  ...attrs
+}: ButtonProps) => {
+  const baseClasses =
+    "inline-flex items-center justify-center gap-1 border-neutral-700 font-medium transition";
+  const defaultClasses = clsx(
+    "rounded-sm border-x-4 border-y px-2 py-1",
+    !loading && "hover:bg-neutral-50"
+  );
+  const filledClasses = clsx(
+    "border bg-neutral-700 px-2 py-0.5 text-white outline outline-1 outline-neutral-700 hover:border-neutral-600 hover:bg-neutral-600 hover:outline-neutral-600"
+  );
+
   return (
     <button
-      {...props}
       className={clsx(
-        "inline-flex items-center justify-center rounded-sm border-x-4 border-y border-neutral-700 px-2 py-1 font-medium transition hover:bg-neutral-50",
-        props.className
+        baseClasses,
+        variant === "default" && defaultClasses,
+        variant === "filled" && filledClasses,
+        className
       )}
-    />
+      disabled={loading}
+      {...attrs}
+    >
+      {children}
+      {loading && <Loader size={20} />}
+    </button>
   );
 };
 
-export const IconButton = (props: ButtonProps) => {
+type IconButtonProps = BaseButtonProps;
+
+export const IconButton = ({
+  children,
+  loading,
+  className,
+  ...attrs
+}: IconButtonProps) => {
   return (
     <button
-      {...props}
       className={clsx(
-        "inline-flex h-6 w-6 items-center justify-center rounded-sm bg-neutral-50 opacity-75 transition hover:bg-neutral-100 hover:opacity-100",
-        props.className
+        "inline-flex h-6 w-6 items-center justify-center rounded-sm bg-neutral-50 opacity-75 transition",
+        !loading && "hover:bg-neutral-100 hover:opacity-100",
+        className
       )}
-    />
+      disabled={loading}
+      {...attrs}
+    >
+      {loading ? <Loader size={20} /> : children}
+    </button>
   );
 };
