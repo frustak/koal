534eaebe8e27eb4c2577d9a12c5f71ee128cc539
frustak <soulburn0017@gmail.com>
2022-12-05 15:30:11 +0330

    add head to all pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
+import Head from "next/head";
 import { AuthChecker } from "../features/auth/auth-checker";
 import { Layout } from "../features/ui/layout";
 import "../styles/globals.css";
@@ -11,13 +12,21 @@ const MyApp: AppType<{ session: Session | null }> = ({
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Layout>
-        <AuthChecker>
-          <Component {...pageProps} />
-        </AuthChecker>
-      </Layout>
-    </SessionProvider>
+    <>
+      <Head>
+        <title>Koal</title>
+        <meta name="description" content="Time management at it's finest" />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+
+      <SessionProvider session={session}>
+        <Layout>
+          <AuthChecker>
+            <Component {...pageProps} />
+          </AuthChecker>
+        </Layout>
+      </SessionProvider>
+    </>
   );
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,17 @@
 import { type NextPage } from "next";
-import Head from "next/head";
 import { Anchor } from "../features/ui/anchor";
 import { Header } from "../features/ui/header";
 
 const Home: NextPage = () => {
   return (
-    <>
-      <Head>
-        <title>Koal</title>
-        <meta name="description" content="Time management at it's finest" />
-        <link rel="icon" href="/favicon.png" />
-      </Head>
-
-      <main>
-        <Header />
-        <div className="flex justify-center">
-          <Anchor href="/plan" className="px-6 py-4 text-2xl">
-            New Day
-          </Anchor>
-        </div>
-      </main>
-    </>
+    <main>
+      <Header />
+      <div className="flex justify-center">
+        <Anchor href="/plan" className="px-6 py-4 text-2xl">
+          New Day
+        </Anchor>
+      </div>
+    </main>
   );
 };
 
