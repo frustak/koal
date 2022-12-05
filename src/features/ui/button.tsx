fc110b80bc5492b53a3baa6752ae3b9ca9769561
frustak <soulburn0017@gmail.com>
2022-12-05 15:27:47 +0330

    improve button style

--- src/features/ui/button.tsx
+++ src/features/ui/button.tsx
@@ -8,7 +8,7 @@ export const Button = (props: ButtonProps) => {
     <button
       {...props}
       className={clsx(
-        "flex items-center justify-center rounded-sm border-x-4 border-y border-neutral-700 px-2 py-1 font-medium hover:bg-neutral-50",
+        "inline-flex items-center justify-center rounded-sm border-x-4 border-y border-neutral-700 px-2 py-1 font-medium transition hover:bg-neutral-50",
         props.className
       )}
     />
