92f44debee8002b342143bd088cafaca9213684e
frustak <soulburn0017@gmail.com>
2022-12-05 16:21:05 +0330

    reduce header margin

--- src/features/ui/header.tsx
+++ src/features/ui/header.tsx
@@ -3,7 +3,7 @@ import { Anchor } from "./anchor";
 
 export const Header = () => {
   return (
-    <header className="mb-10 flex items-start justify-between">
+    <header className="mb-6 flex items-start justify-between">
       <Anchor href="/">KOAL</Anchor>
       <div className="flex justify-end">
         <Profile />
