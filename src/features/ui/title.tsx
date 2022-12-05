656721309dfeca54c4a998ffdd3ca1c1514ee354
frustak <soulburn0017@gmail.com>
2022-12-05 17:57:32 +0330

    implement title component

---
+++ src/features/ui/title.tsx
@@ -0,0 +1,7 @@
+import type { ReactNode } from "react";
+
+export const Title = ({ children }: { children: ReactNode }) => {
+  return (
+    <h2 className="mb-6 text-xl underline underline-offset-4">{children}</h2>
+  );
+};
