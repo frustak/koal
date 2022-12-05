45132a2b402feb2eac666c9f7d540adda3ea4488
frustak <soulburn0017@gmail.com>
2022-12-05 15:27:57 +0330

    implement anchor

---
+++ src/features/ui/anchor.tsx
@@ -0,0 +1,23 @@
+import clsx from "clsx";
+import Link from "next/link";
+import type { ReactNode } from "react";
+
+type AnchorProps = {
+  children: ReactNode;
+  href: string;
+  className?: string;
+};
+
+export const Anchor = ({ children, href, className }: AnchorProps) => {
+  return (
+    <Link
+      href={href}
+      className={clsx(
+        "inline-flex rounded-sm bg-neutral-700 p-1 text-xs font-medium text-white transition hover:-rotate-3",
+        className
+      )}
+    >
+      {children}
+    </Link>
+  );
+};
