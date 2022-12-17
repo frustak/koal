22d5ec84bcf0130da3de11921c29a4e18c016704
frustak <soulburn007@gmail.com>
2022-12-17 18:28:32 +0330

    fetch goals in goal list

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,7 @@ import { GoalForm } from "../features/goal/form";
 import { GoalList } from "../features/goal/list";
 import { Anchor } from "../features/ui/anchor";
 import { Button } from "../features/ui/button";
-import { Loader } from "../features/ui/loader";
 import { Title } from "../features/ui/title";
-import { trpc } from "../utils/trpc";
 
 const Home: NextPage = () => {
   return (
@@ -24,17 +22,13 @@ const Home: NextPage = () => {
 
 export const GoalsSection = () => {
   const [isAddingGoal, setIsAddingGoal] = useState(false);
-  const goalsQuery = trpc.todo.getGoals.useQuery();
-  const goals = goalsQuery.data?.goals ?? [];
   const onClickAddGoal = () => setIsAddingGoal(true);
   const onCreateGoal = () => setIsAddingGoal(false);
-
-  if (goalsQuery.isLoading) return <Loader />;
 
   return (
     <div>
       <Title>Goals</Title>
-      <GoalList goals={goals} />
+      <GoalList />
       <div className="mt-6">
         {isAddingGoal && <GoalForm onSuccess={onCreateGoal} />}
         {!isAddingGoal && <Button onClick={onClickAddGoal}>Add Goal</Button>}
