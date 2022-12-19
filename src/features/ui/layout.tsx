a78e6ba51d8099cbd2b94a248d866222884bb5ae
frustak <soulburn007@gmail.com>
2022-12-19 16:32:56 +0330

    improve text selection color

--- src/features/ui/layout.tsx
+++ src/features/ui/layout.tsx
@@ -10,7 +10,7 @@ export const Layout = ({ children }: { children: ReactNode }) => {
         <link rel="icon" href="/favicon.png" />
       </Head>
 
-      <div className="container mx-auto flex h-screen flex-col p-8 font-display text-neutral-700 selection:bg-neutral-700 selection:text-white">
+      <div className="container mx-auto flex h-screen flex-col p-8 font-display text-neutral-700 selection:bg-neutral-900 selection:text-white">
         <div className="flex grow flex-col rounded-sm border-4 border-neutral-700 px-6 py-4">
           {children}
         </div>
