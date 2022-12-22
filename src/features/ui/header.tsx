bf4c10a5e67517a18972bb06796e4298d0c07205
frustak <soulburn007@gmail.com>
2022-12-22 19:34:16 +0330

    add bottom border to header

--- src/features/ui/header.tsx
+++ src/features/ui/header.tsx
@@ -3,7 +3,7 @@ import { Anchor } from "./anchor";
 
 export const Header = () => {
   return (
-    <header className="mb-6 flex items-start justify-between">
+    <header className="mb-6 flex items-start justify-between border-b-4 border-neutral-700 px-6 pb-4">
       <Anchor href="/">KOAL</Anchor>
       <div className="flex justify-end">
         <Profile />
