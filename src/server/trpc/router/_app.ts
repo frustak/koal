c0f3ec7bc770d91f9b4abbe199e8a5fb9f71a8c2
frustak <soulburn007@gmail.com>
2022-12-22 19:35:55 +0330

    export planning router

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,10 +1,12 @@
 import { router } from "../trpc";
 import { authRouter } from "./auth";
+import { planningRouter } from "./planning";
 import { todoRouter } from "./todo";
 
 export const appRouter = router({
   auth: authRouter,
   todo: todoRouter,
+  planning: planningRouter,
 });
 
 // export type definition of API
