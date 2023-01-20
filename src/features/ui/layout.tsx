d33b18717aa7070c1b5be125f58eb572ee52b047
frustak <soulburn007@gmail.com>
2023-01-20 19:57:22 +0330

    improve favicon

--- src/features/ui/layout.tsx
+++ src/features/ui/layout.tsx
@@ -10,7 +10,24 @@ export const Layout = ({ children }: { children: ReactNode }) => {
                     name="description"
                     content="Time management at it's finest"
                 />
-                <link rel="icon" href="/favicon.png" />
+                <link
+                    rel="apple-touch-icon"
+                    sizes="180x180"
+                    href="/apple-touch-icon.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="32x32"
+                    href="/favicon-32x32.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="16x16"
+                    href="/favicon-16x16.png"
+                />
+                <link rel="manifest" href="/site.webmanifest" />
             </Head>
 
             <div className="container mx-auto flex h-screen flex-col p-8 font-display text-neutral-700 selection:bg-neutral-900 selection:text-white">
