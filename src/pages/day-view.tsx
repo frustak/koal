2b025147fa50e15adf8396a3427ef395b6214390
frustak <soulburn007@gmail.com>
2023-01-24 17:57:06 +0330

    add focus section to day view

--- src/pages/day-view.tsx
+++ src/pages/day-view.tsx
@@ -1,11 +1,27 @@
+import clsx from "clsx";
+import { format, isAfter, isBefore } from "date-fns";
+import _ from "lodash";
 import type { NextPage } from "next";
 import { TodoItem } from "../features/todo/list";
 import { Loader } from "../features/ui/loader";
 import { Title } from "../features/ui/title";
 import { trpc } from "../utils/trpc";
-import _ from "lodash";
 
 const DayViewPage: NextPage = () => {
+    return (
+        <main className="grid grow grid-cols-4 divide-x-2 divide-neutral-700">
+            <div className="col-span-3 pr-6 pt-4">
+                <TodosSection />
+            </div>
+
+            <div className="pt-4">
+                <FocusSection />
+            </div>
+        </main>
+    );
+};
+
+const TodosSection = () => {
     const inboxQuery = trpc.planning.inbox.useQuery();
     const goalToTodos = inboxQuery.data?.goalToTodos ?? [];
     const focusGoal = inboxQuery.data?.focusGoal.name;
@@ -17,7 +33,7 @@ const DayViewPage: NextPage = () => {
         );
 
     return (
-        <main className="flex grow flex-col">
+        <div>
             {_.toPairs(goalToTodos).map(([goal, todos]) => (
                 <div key={goal}>
                     <Title>{goal == focusGoal ? `Focus ${goal}` : goal}</Title>
@@ -28,7 +44,60 @@ const DayViewPage: NextPage = () => {
                     </ul>
                 </div>
             ))}
-        </main>
+        </div>
+    );
+};
+
+const FocusSection = () => {
+    const inboxQuery = trpc.planning.inbox.useQuery();
+    const focusTime = inboxQuery.data?.focusTime;
+    const now = new Date();
+    const start = focusTime?.start ?? now;
+    const end = focusTime?.end ?? now;
+    const focused = isBefore(now, end) && isAfter(now, start);
+
+    if (inboxQuery.isLoading) return <Loader />;
+
+    return (
+        <div className="flex flex-col items-center">
+            <Title>Focus Time</Title>
+            <div
+                className={clsx(
+                    "rounded-sm px-6 py-4",
+                    focused && "bg-neutral-700 text-white"
+                )}
+            >
+                <div className="space-y-6 text-center">
+                    <FocusTime focused={focused} label="start" time={start} />
+                    <FocusTime focused={focused} label="end" time={end} />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+const FocusTime = ({
+    label,
+    time,
+    focused,
+}: {
+    label: string;
+    time: Date;
+    focused: boolean;
+}) => {
+    return (
+        <div>
+            <h4
+                className={clsx(
+                    "text-xs",
+                    focused && "text-neutral-300",
+                    !focused && "text-neutral-500"
+                )}
+            >
+                {label}
+            </h4>
+            <p>{format(time, "kk:mm")}</p>
+        </div>
     );
 };
 
