5726ded79b8b3c8f3d93c5600f9528e96b1336cf
frustak <soulburn007@gmail.com>
2023-02-21 13:39:10 +0330

    remove add todo button and always show  form

--- src/features/todo/section.tsx
+++ src/features/todo/section.tsx
@@ -1,14 +1,8 @@
-import { useState } from "react";
-import { Button } from "../ui/button";
 import { Title } from "../ui/title";
 import { TodoForm } from "./form";
 import { TodosList } from "./list";
 
 export const TodosSection = ({ goalId }: { goalId: string | null }) => {
-    const [isAddingTodo, setIsAddingTodo] = useState(false);
-    const onClickAddTodo = () => setIsAddingTodo(true);
-    const onAddTodo = () => setIsAddingTodo(false);
-
     return (
         <div className="flex flex-col">
             <Title>Todos</Title>
@@ -16,12 +10,7 @@ export const TodosSection = ({ goalId }: { goalId: string | null }) => {
             {goalId && <TodosList goalId={goalId} />}
             {goalId && (
                 <div className="mt-6">
-                    {isAddingTodo && (
-                        <TodoForm goalId={goalId} onSuccess={onAddTodo} />
-                    )}
-                    {!isAddingTodo && (
-                        <Button onClick={onClickAddTodo}>Add Todo</Button>
-                    )}
+                    <TodoForm goalId={goalId} />
                 </div>
             )}
         </div>
