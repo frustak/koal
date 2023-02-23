16c63123b342650dd73d2389185dc272243bd6ab
frustak <soulburn007@gmail.com>
2023-02-23 16:16:28 +0330

    refactor someday page

--- src/pages/someday.tsx
+++ src/pages/someday.tsx
@@ -1,22 +1,17 @@
-import _ from "lodash";
 import type { NextPage } from "next";
-import { TodoItem } from "../features/todo/list";
+import { TodosList } from "../features/todo/list";
 import { trpc } from "../utils/trpc";
 
 const SomedayPage: NextPage = () => {
     const todoQuery = trpc.todo.getTodos.useQuery({ priority: "not_urgent" });
-    const notUrgentTasks = todoQuery.data?.todos;
-    if (!notUrgentTasks) {
-        return <div> You are a true hero. Nothing to do. </div>;
-    }
+    const notUrgentTasks = todoQuery.data?.todos ?? [];
+
     return (
-        <div>
-            <ul>
-                {_.forEach(notUrgentTasks).map((todo) => (
-                    <TodoItem key={todo.id} todo={todo}></TodoItem>
-                ))}
-            </ul>
-        </div>
+        <TodosList
+            todos={notUrgentTasks}
+            loading={todoQuery.isLoading}
+            emptyMessage="You are a true hero. Nothing to do."
+        />
     );
 };
 
