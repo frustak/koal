288a332c48a1e403d80c6a361ae87a71a2b8cacc
frustak <soulburn007@gmail.com>
2023-03-25 16:35:42 +0430

    add move todo buttons

--- src/features/todo/list.tsx
+++ src/features/todo/list.tsx
@@ -1,5 +1,5 @@
 import clsx from "clsx";
-import { Check, Eraser } from "phosphor-react";
+import { CaretDown, CaretUp, Check, Eraser } from "phosphor-react";
 import type { Todo } from "../../server/trpc/router/todo";
 import { trpc } from "../../utils/trpc";
 import { IconButton } from "../ui/button";
@@ -10,24 +10,64 @@ export const TodosList = ({
     todos,
     loading,
     emptyMessage = "Nothing to do but chill, stay frosty ❄️",
+    withMove,
 }: {
     todos: Todo[];
     loading?: boolean;
     emptyMessage?: string;
+    withMove?: boolean;
 }) => {
+    const ordersMutation = trpc.todo.updateOrders.useMutation();
+
+    const move = (todoId: string, direction: "up" | "down") => {
+        const index = todos.findIndex((todo) => todo.id === todoId);
+        const targetIndex = direction === "up" ? index - 1 : index + 1;
+        const newTodos = [...todos];
+        const todo = newTodos[index];
+        const otherTodo = newTodos[targetIndex];
+        if (!todo || !otherTodo) return;
+        newTodos[index] = otherTodo;
+        newTodos[targetIndex] = todo;
+        const todoIds = newTodos.map((todo) => todo.id);
+        console.log(newTodos.map((todo) => todo.title));
+        ordersMutation.mutate({ todoIds });
+    };
+
     if (loading) return <Loader />;
     if (todos.length === 0) return <Subtitle>{emptyMessage}</Subtitle>;
 
     return (
-        <ul className="space-y-3">
-            {todos.map((todo) => (
-                <TodoItem key={todo.id} todo={todo} />
+        <ul>
+            {todos.map((todo, index) => (
+                <TodoItem
+                    key={todo.id}
+                    todo={todo}
+                    move={move}
+                    isFirst={index === 0}
+                    isLast={index === todos.length - 1}
+                    orderLoading={ordersMutation.isLoading}
+                    withMove={withMove}
+                />
             ))}
         </ul>
     );
 };
 
-export const TodoItem = ({ todo }: { todo: Todo }) => {
+export const TodoItem = ({
+    todo,
+    move,
+    isFirst,
+    isLast,
+    orderLoading,
+    withMove,
+}: {
+    todo: Todo;
+    move: (todoId: string, direction: "up" | "down") => void;
+    isFirst: boolean;
+    isLast: boolean;
+    orderLoading: boolean;
+    withMove?: boolean;
+}) => {
     const updateMutation = trpc.todo.updateTodoStatus.useMutation();
     const deleteMutation = trpc.todo.deleteTodo.useMutation();
     const isDone = !!todo.isDone;
@@ -37,9 +77,15 @@ export const TodoItem = ({ todo }: { todo: Todo }) => {
     const deleteTodo = () => {
         deleteMutation.mutate({ todoId: todo.id });
     };
+    const moveUp = () => {
+        move(todo.id, "up");
+    };
+    const moveDown = () => {
+        move(todo.id, "down");
+    };
 
     return (
-        <li className="flex items-center justify-between gap-6">
+        <li className="group flex items-center justify-between gap-6 rounded-sm py-1 px-2 hover:bg-neutral-50">
             <div className="flex grow items-center gap-4">
                 <IconButton
                     onClick={toggleDone}
@@ -56,9 +102,37 @@ export const TodoItem = ({ todo }: { todo: Todo }) => {
                     </p>
                 </div>
             </div>
-            <IconButton onClick={deleteTodo} loading={deleteMutation.isLoading}>
-                <Eraser weight="duotone" />
-            </IconButton>
+            <div className="invisible flex items-center gap-2 group-hover:visible">
+                <div
+                    className={clsx(
+                        "flex flex-col gap-1",
+                        !withMove && "invisible"
+                    )}
+                >
+                    <IconButton
+                        className="h-4 w-4 text-xs"
+                        onClick={moveUp}
+                        disabled={isFirst}
+                        loading={orderLoading}
+                    >
+                        <CaretUp weight="duotone" />
+                    </IconButton>
+                    <IconButton
+                        className="h-4 w-4 text-xs"
+                        onClick={moveDown}
+                        disabled={isLast}
+                        loading={orderLoading}
+                    >
+                        <CaretDown weight="duotone" />
+                    </IconButton>
+                </div>
+                <IconButton
+                    onClick={deleteTodo}
+                    loading={deleteMutation.isLoading}
+                >
+                    <Eraser weight="duotone" />
+                </IconButton>
+            </div>
         </li>
     );
 };
--- src/features/todo/section.tsx
+++ src/features/todo/section.tsx
@@ -3,12 +3,18 @@ import { Subtitle, Title } from "../ui/title";
 import { TodoForm } from "./form";
 import { TodosList } from "./list";
 
-export const TodoSection = ({ goalId }: { goalId: string | null }) => {
+export const TodoSection = ({
+    goalId,
+    withMove,
+}: {
+    goalId: string | null;
+    withMove?: boolean;
+}) => {
     return (
         <div className="flex flex-col">
             <Title>Todos</Title>
             {!goalId && <Subtitle>Select a goal to see todos</Subtitle>}
-            {goalId && <AllTodoList goalId={goalId} />}
+            {goalId && <AllTodoList goalId={goalId} withMove={withMove} />}
             {goalId && (
                 <div className="mt-6">
                     <TodoForm goalId={goalId} />
@@ -18,11 +24,23 @@ export const TodoSection = ({ goalId }: { goalId: string | null }) => {
     );
 };
 
-const AllTodoList = ({ goalId }: { goalId: string }) => {
+const AllTodoList = ({
+    goalId,
+    withMove,
+}: {
+    goalId: string;
+    withMove?: boolean;
+}) => {
     const todosQuery = trpc.todo.getTodos.useQuery({
         goalIds: [goalId],
     });
     const todos = todosQuery.data?.todos ?? [];
 
-    return <TodosList todos={todos} loading={todosQuery.isLoading} />;
+    return (
+        <TodosList
+            todos={todos}
+            loading={todosQuery.isLoading}
+            withMove={withMove}
+        />
+    );
 };
